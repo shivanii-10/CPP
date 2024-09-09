08c7bc9e5248aa813da3d6ecf93e305a1b0e0cde
shivanii-10 <shivanianushka144@gmail.com>
2024-09-09 18:08:55 +0530

    patterns practice

---
+++ pattern3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//j=column
+//i=row
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+    int x=i;
+        while(j<=i){
+            cout<<x<<" ";
+           x=x-1;
+            j=j+1;
+        }
+        i=i+1;
+        cout<<endl;
+    }
+}
